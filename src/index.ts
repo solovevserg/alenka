5aa3d3224ef4f07cbf1c024ff8bda58fdcb2c018
Sergei Solovev <sergey.soloviev@inbox.ru>
2022-05-15 01:14:07 +0300

    code refactroing and library exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import { NamePredictor } from "./name-predictor";
+import { Gender } from "./types/gender";
+import { Name } from "./types/name";
+import { Options } from "./types/options";
 
 export {
     NamePredictor,
+    Gender,
+    Name,
+    Options,
 }
